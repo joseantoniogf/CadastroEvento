d588e98ee55e92acd3d2a0f0f999d313996450ab
joseantoniogf <joseantoniio.gf@gmail.com>
2021-08-28 15:23:56 -0300

    CadastroEvento

---
+++ Evento.js
@@ -0,0 +1,20 @@
+console.log("Bem-vindo ao Evento");
+
+let data = "28/08/2021";
+if (data <= "29/08/2021"); {
+    console.log("Impossível cadastrar. Data inválida");
+}
+let cadastrados = ["José" , "Maria" , "João" , "Gabriela" , "Lucas" , "Ana"];
+let numeroDeCadastrados = cadastrados.length;
+if (cadastrados.length <= 100) {
+    console.log("Digite seu nome"); 
+    console.log("Digite sua idade");
+    
+let idade = 18;
+if (idade >= 18) {
+    console.log("Cadastro realizado!");
+} else {
+    console.log("Idade não permitida!"); }
+} else {
+    console.log("Este cadastro excede o número máximo de participantes permitido. Cadastro não realizado!");
+}
